621d75f442164317e551cd7b7628d9c5fc536ccf
Mehdi Assefi <asf@uga.edu>
2021-09-19 15:47:01 -0400

    Add files via upload

--- databaseui/main.js
+++ databaseui/main.js
@@ -1,220 +1,101 @@
-function getValue(inputSelector) {
-    var text = document.querySelector(inputSelector).value;
-    switch (text) {
-        case 'none':
-            return;
-        case 'tab':
-            return '\t';
-        default:
-            return text;
-    }
-}
+  var dateFilterParams = {
+      comparator: function(filterLocalDateAtMidnight, cellValue) {
+          var dateAsString = cellValue;
+          if (dateAsString == null) return -1;
+          var dateParts = dateAsString.split('/');
+          var cellDate = new Date(
+              Number(dateParts[2]),
+              Number(dateParts[1]) - 1,
+              Number(dateParts[0])
+          );
 
-function getParams() {
-    return {
-        columnSeparator: getValue('#columnSeparator'),
-    };
-}
+          if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+              return 0;
+          }
 
-function onBtnExport() {
-    var params = getParams();
-    if (params.columnSeparator) {
-        alert(
-            'NOTE: you are downloading a file with non-standard separators - it may not render correctly in Excel.'
-        );
-    }
-    gridOptions.api.exportDataAsCsv(params);
-}
+          if (cellDate < filterLocalDateAtMidnight) {
+              return -1;
+          }
 
-function onBtnUpdate() {
-    document.querySelector('#csvResult').value = gridOptions.api.getDataAsCsv(
-        getParams()
-    );
-}
+          if (cellDate > filterLocalDateAtMidnight) {
+              return 1;
+          }
+      },
+      browserDatePicker: true,
+  };
 
-// setup the grid after the page has finished loading
-
-
-var dateFilterParams = {
-    comparator: function(filterLocalDateAtMidnight, cellValue) {
-        var dateAsString = cellValue;
-        if (dateAsString == null) return -1;
-        var dateParts = dateAsString.split('/');
-        var cellDate = new Date(
-            Number(dateParts[2]),
-            Number(dateParts[1]) - 1,
-            Number(dateParts[0])
-        );
-
-        if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
-            return 0;
-        }
-
-        if (cellDate < filterLocalDateAtMidnight) {
-            return -1;
-        }
-
-        if (cellDate > filterLocalDateAtMidnight) {
-            return 1;
-        }
-    },
-    browserDatePicker: true,
-};
-
-columnDefs = [
-    { field: 'NAME', filter: 'agTextColumnFilter', suppressMenu: true },
-    {
-        field: 'CENSUS2010POP',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'ESTIMATESBASE2010',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2010',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2011',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2012',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2013',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2014',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2015',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2016',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2017',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2018',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
-
-    {
-        field: 'POPESTIMATE2019',
-        filterParams: {
-            allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
-            numberParser: text => {
-                return text == null ? null : parseFloat(text.replace(',', '.'));
-            }
-        }
-    },
+  columnDefs = [
+      { field: 'Isolate_Id', suppressMenu: true },
+      { field: 'Isolate_Name', filter: 'agTextColumnFilter', suppressMenu: true },
+      { field: 'Location', suppressMenu: true, filter: 'agTextColumnFilter' },
+      { field: 'HA Segment_Id', filter: false },
+      {
+          field: 'collection_date',
+          filter: 'agNumberColumnFilter',
+          filterParams: {
+              allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
+              numberParser: text => {
+                  return text == null ? null : parseFloat(text.replace(',', '.'));
+              }
+          }
+      },
+      {
+          field: 'Host_Age',
+          filter: 'agNumberColumnFilter',
+          filterParams: {
+              allowedCharPattern: '\\d\\-\\,', // note: ensure you escape as if you were creating a RegExp from a string
+              numberParser: text => {
+                  return text == null ? null : parseFloat(text.replace(',', '.'));
+              }
+          }
+      },
+  ];
 
 
+  var gridOptions = {
+      columnDefs: columnDefs,
+      defaultColDef: {
+          flex: 1,
+          minWidth: 150,
+          filter: true,
+          sortable: true,
+          floatingFilter: true,
+      },
+  };
 
-];
+  function irelandAndUk() {
+      var countryFilterComponent = gridOptions.api.getFilterInstance('country');
+      countryFilterComponent.setModel({ values: ['Ireland', 'Great Britain'] });
+      gridOptions.api.onFilterChanged();
+  }
 
 
-var gridOptions = {
-    columnDefs: columnDefs,
-    defaultColDef: {
-        flex: 1,
-        minWidth: 150,
-        filter: true,
-        sortable: true,
-        floatingFilter: true,
-    },
-    suppressExcelExport: true,
-    popupParent: document.body,
+  function endingStan() {
+      var countryFilterComponent = gridOptions.api.getFilterInstance('country');
+      var countriesEndingWithStan = countryFilterComponent
+          .getValues()
+          .filter(function(value) {
+              return value.indexOf('stan') === value.length - 4;
+          });
 
-};
+      countryFilterComponent.setModel({ values: countriesEndingWithStan });
+      gridOptions.api.onFilterChanged();
+  }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    // const params = new URLSearchParams(window.location.search)
-    // const fileName = params.get('ds');
+  // setup the grid after the page has finished loading
+  document.addEventListener('DOMContentLoaded', function() {
+      // const params = new URLSearchParams(window.location.search)
+      // const fileName = params.get('ds');
 
-    var gridDiv = document.querySelector('#myGrid');
-    new agGrid.Grid(gridDiv, gridOptions);
+      var gridDiv = document.querySelector('#myGrid');
+      new agGrid.Grid(gridDiv, gridOptions);
 
-    agGrid
-        .simpleHttpRequest({
-            url: '/data/' + document.getElementById('dataFileName').value // '/data/CDC_H3_HA_seq_meta_sample.json',
-        })
-        .then(function(data) {
-            gridOptions.api.setRowData(data);
-        });
-});
+      agGrid
+          .simpleHttpRequest({
+              url: '/data/' + document.getElementById('dataFileName').value // '/data/CDC_H3_HA_seq_meta_sample.json',
+          })
+          .then(function(data) {
+              gridOptions.api.setRowData(data);
+          });
+  });
